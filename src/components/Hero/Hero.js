6c086c261ce3864a288222b31da4e162ee2c095e
Praveen Karthik <praveenkarthik@example.com>
2026-01-19 14:03:13 +0530

    Renamed to Praveen Karthik and updated links

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { LeftSection } from "./HeroStyles";
+import { FiverUrl, ResumeUrl, UpworkUrl } from "../../constants/constants";
+import LinkButton from "../../styles/GlobalComponents/LinkButton";
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hello! <br />
+          I'm Praveen Karthik
+        </SectionTitle>
+        <SectionText>
+          A Full Stack Backend & Solana Blockchain Developer specializing in
+          scalable server-side applications with Node.js & Rust. Proficient in
+          Next.js and React.js for seamless web development.
+        </SectionText>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "10px",
+            margin: "0 0 80px",
+          }}
+        >
+          <LinkButton alt="upwork" href={UpworkUrl} target="_blank">
+            Upwork
+          </LinkButton>
+          <LinkButton alt="fiverr" href={FiverUrl} target="_blank">
+            Fiverr
+          </LinkButton>
+          <LinkButton alt="resume" href={ResumeUrl} target="_blank">
+            Resume
+          </LinkButton>
+        </div>
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,254 @@
+export const projects = [
+  {
+    title: "Honeycomb Protocol",
+    description:
+      "The development of Web3 applications in gaming is often seen as overly complex, leading to a lack of specialized tools for game developers.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fhoneycomb2.png?alt=media&token=0e607a9e-98e8-4e47-b9ed-4d1eeec81e11",
+    tags: ["Rust", "Node.js"],
+    visit: null,
+    source: "https://docs.honeycombprotocol.com/",
+    id: 52,
+  },
+  {
+    title: "Proleague",
+    description:
+      "A web 3.0 platform on the Solana blockchain for hosting tournaments, enabling game sponsorships, and providing instant payouts for games.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproleague2.png?alt=media&token=41fbca51-fd49-4130-a364-da90f96c25fd",
+    tags: ["Rust", "Node.js", "Next.js", "MongoDB"],
+    visit: null,
+    source: "https://alpha.proleague.gg/",
+    id: 42,
+  },
+  {
+    title: "The PartyCade",
+    description:
+      "The Partycade offers unique games for many occasions. Such as Birthday Dinners, Baby Showers, Bridal Showers and many more. You’ll get to enjoy a friendly fun competition with your loved ones all from your smart device. Can’t make it to the event? No worries, you can join the game room wherever you are.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2FpartycadeMain.png?alt=media&token=472f620d-3322-44da-97ac-5e3caa5d94a9",
+    // image: "/images/jp01.jpg",
+    tags: ["Node.js", "Prisma", "Flutter", "GraphQL", "Pubsub"],
+
+    source:
+      "https://play.google.com/store/apps/details?id=com.thepartycade.partycade",
+    visit: null,
+    id: 22,
+  },
+  {
+    title: "Scribble",
+    description:
+      "Scribble Library. Showcase your work on our exclusive Scribble Library for all to see! Once you submit your information, we will post your submitted book cover and information in the Scribble Library Directory to be available for all users to see, read, rate and review!",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fjp01.jpg?alt=media&token=b9076390-4516-4fd3-b55d-64811f0dce2c",
+    // image: "/images/jp01.jpg",
+    tags: ["Node.js", "Microservices", "Flutter", "Socket.io", "MongoDB"],
+
+    source:
+      "https://play.google.com/store/apps/details?id=com.scribble.thesuitch",
+    visit: null,
+    id: 32,
+  },
+  {
+    title: "Scribble Dashboard",
+    description:
+      "Introducing the 'Scribble Dashboard,' a powerful CRM tool designed specifically for managing submitted book covers and information. With the Scribble Dashboard, users have complete control over the book submission process, ensuring seamless organization and efficient management.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%20001.png?alt=media&token=130ea4e6-d6fc-4381-b65a-9e9c6722b001",
+    // image: "/images/project 001.png",
+    tags: ["Next.js", "Microservices", "Node.js", "Socket.io", "MongoDB"],
+
+    source: "https://dashboard.thescribbleapp.com",
+    visit: null,
+    id: 35,
+  },
+  {
+    title: "KC-Elite",
+    description:
+      "Elevate your basketball skills with the KC Elite Basketball Training Mobile App. Personalized workouts, expert coaching, progress tracking, and convenient scheduling all in one place. Reach new heights on the court. Unlock your full potential and take your basketball game to the next level.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fjp02.png?alt=media&token=b25fa786-1c19-4c58-9954-106b0a4737aa",
+    // image: "/images/jp02.png",
+    tags: ["Node.js", "Monolithic Arc", "Flutter", "MongoDB"],
+
+    source:
+      "https://play.google.com/store/apps/details?id=com.kcebasketball.kcelite",
+    visit: null,
+    id: 33,
+  },
+  {
+    title: "Health Titan Pro",
+    description:
+      "Introducing Health Titan Pro, the online COVID vaccination center where you can fill out a form and get vaccinated within minutes. All the results will be sent to your email. Experience the convenience and speed of Health Titan Pro for your vaccination needs.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fhtp001.png?alt=media&token=ef567442-01ec-474f-be8f-e4791379300e",
+    // image: "/images/htp001.png",
+    tags: ["React", "Node.js", "Dropbox", "MongoDB"],
+
+    source: "https://project-htp.web.app/",
+    visit: null,
+    id: 47,
+  },
+  {
+    title: "NFT Market-place (Dreamhub)",
+    description:
+      "Dreamhub is an Ethereum-based NFT market-place that allows creators to mint and sell their NFTs. Additionally, creators will be able to place their NFTs on the market for bidding and auction",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fssd01.png?alt=media&token=9a9ec35e-5c7f-4d40-bd40-bcbf6f2dd058",
+    // image: "/images/ssd01.png",
+    tags: ["React", "Node.js", "MetaMask", "Web3.js", "MongoDB"],
+    source: "https://dreamhub.art",
+    visit: null,
+    id: 40,
+  },
+  {
+    title: "Women-Tribe",
+    description:
+      "A thriving community that promotes inclusivity, education, empathy, and empowerment for women. To celebrate Pride Month, we have created our official Women Tribe Pride NFT! Establishing and funding a yearly scholarship program to help women in",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fssd02.png?alt=media&token=94475244-55be-410e-b642-75100d1bc1b3",
+    // image: "/images/ssd02.png",
+    tags: ["React", "CSS3", "Bootstrap"],
+
+    source: "https://womentribe.art/",
+    visit: null,
+    id: 41,
+  },
+  {
+    title: "Alamal Computers",
+    description:
+      "Welcome to Alamal Computer, the ultimate e-commerce platform dedicated to laptops, computers, and accessories. At Alamal Computer, we provide a seamless marketplace where both sellers and consumers can easily buy and sell items. Our platform supports secure transactions through PayPal.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%208.png?alt=media&token=de7e5f7a-f54b-403d-bec8-f2d36b630bf2",
+    // image: "/images/project 8.png",
+    tags: ["React", "Firebase", "Node.js", "Paypal", "MongoDB"],
+
+    source: "https://alamal-computer.onrender.com",
+    visit: "https://github.com/praveenkarthik17/Alamal-Computers",
+    id: 1,
+  },
+  // {
+  //   title: "Mini-FB",
+  //   description:
+  //     "Introducing 'MiniFb', a feature-rich web application that revolutionizes social networking. With Minifb, users can effortlessly post updates in real-time, delete their posts at their convenience, and explore their personalized profiles for a truly immersive social experience.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%207.png?alt=media&token=f554457d-6ca6-4d9b-a72b-5f36891258a2",
+  //   // image: "/images/project 7.png",
+  //   tags: ["React", "Firebase", "Node.js", "Socket.io", "MongoDB"],
+
+  //   source: "https://minifb.onrender.com",
+  //  visit: "https://github.com/praveenkarthik17/miniFB",
+  //   id: 1,
+  // },
+  // {
+  //   title: "Postman",
+  //   description:
+  //     "Introducing a simplified Postman clone, designed specifically for web developers to effortlessly test and validate their APIs. This user-friendly web application has been extensively modified for seamless usability and offers a straightforward interface.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%201.png?alt=media&token=528b7ed5-a396-4062-bb75-72a3ea998bad",
+  //   // image: "/images/project 1.png",
+  //   tags: ["HTML5", "CSS3", "JavaScript"],
+
+  //   source: "https://postman-praveenkarthik.web.app",
+  //  visit: "https://github.com/praveenkarthik17/postman",
+  //   id: 1,
+  // },
+  // {
+  //   title: "Text-Utils App",
+  //   description:
+  //     "Check out this unique app designed for real-time checking of spaces, words, and characters, among other features. Experience its special attributes, including dark mode and multiple color modes, which enhance your user experience.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%205.png?alt=media&token=851cdde4-0df6-44e3-b0d8-7bff5728313f",
+  //   // image: "/images/project 5.png",
+  //   tags: ["React", "Javascript", "Bootstrap"],
+  //  source: "https://praveenkarthik17.github.io/React-TextUtils-App",
+  //  visit: "https://github.com/praveenkarthik17/React-TextUtils-App",
+  //   id: 4,
+  // },
+  // {
+  //   title: "Todo App",
+  //   description:
+  //     "Task management made easy with this popular mobile app. Remember and organize your daily activities effortlessly. Streamline your productivity and stay on top of your tasks.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%203.png?alt=media&token=37b7e931-edd1-4c66-bd8b-71dc7688e925",
+  //   // image: "/images/project 3.png",
+  //   tags: ["React", "Material-UI", "Firebase"],
+  //  source: "https://praveenkarthik17.github.io/React-Todo-App",
+  //  visit: "https://github.com/praveenkarthik17/React-Todo-App",
+  //   id: 3,
+  // },
+  // {
+  //   title: "Weather App",
+  //   description:
+  //     "Discover the Weather app to search and view instant weather details for cities worldwide. Get insights into Nature, Temperature, Wind, Humidity, and Country information with a single click.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%204.png?alt=media&token=678f6f6d-5ec8-467b-a9ab-e0d27720d30f",
+  //   // image: "/images/project 4.png",
+  //   tags: ["React", "Javascript", "React-Bootstrap"],
+
+  //  source: "https://praveenkarthik17.github.io/weatherBootstrap",
+  //  visit: "https://github.com/praveenkarthik17/weatherBootstrap",
+  //   id: 2,
+  // },
+  // {
+  //   title: "Quiz Web-app",
+  //   description:
+  //     "A unique Quiz web app with special functionalities where users can view real-time answers and calculate their results instantly.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%206.png?alt=media&token=a6e8a781-d090-4a70-81e9-8623692c3956",
+  //   // image: "/images/project 6.png",
+  //   tags: ["React", , "Javascript", "Bootstrap"],
+  //  source: "https://praveenkarthik17.github.io/Asinlab-Interview-Test",
+  //  visit: "https://github.com/praveenkarthik17/Asinlab-Interview-Test",
+  //   id: 6,
+  // },
+  // {
+  //   title: "React Router App",
+  //   description:
+  //     "Check out my simple React Router app with attractive and visually appealing route definitions.",
+  //   image:
+  //     "https://firebasestorage.googleapis.com/v0/b/bucket-a2d0b.appspot.com/o/postImages%2Fproject%202.png?alt=media&token=c4c5c4c6-6ae0-4fdc-bf56-0ef42eee035e",
+  //   // image: "/images/project 2.png",
+  //   tags: ["React", , "Javascript", "Material-UI"],
+  //   source: "https://numberless-glove.surge.sh",
+  //  visit: "https://github.com/praveenkarthik17/React-Router",
+  //   id: 0,
+  // },
+];
+
+export const TimeLineData = [
+  // { year: 2017, text: "Passed my Matriculation in Computer Science" },
+  // { year: 2019, text: "Passed my Intermediate in Pre-Engineering" },
+  {
+    year: 2020,
+    text: "Enrolled in university for a Bachelor's in Computer Science",
+  },
+  {
+    year: 2021,
+    text: "Started as a freelance and onsite developer, embarking on my industry journey",
+  },
+  {
+    year: 2022,
+    text: "I have learned and struggled a lot. Difficult times make a person stronger",
+  },
+  {
+    year: 2023,
+    text: "Completed my bachelors degree",
+  },
+  {
+    year: 2024,
+    text: "Focusing on backend development using Node.js and Solana blockchain.",
+  },
+];
+
+export const FiverUrl = "https://www.fiverr.com/tanv33r_khan";
+export const UpworkUrl =
+  "https://www.upwork.com/freelancers/~0128ca58b87e6d95b6";
+export const ResumeUrl = "https://www.cakeresume.com/tanveer-khan-21f68d";
+export const GithubUrl = "https://github.com/praveenkarthik17";
+export const LinkedInUrl =
+  "https://www.linkedin.com/in/praveen-k-70419124a/";
+export const FacebookUrl =
+  "https://www.facebook.com/profile.php?id=100007686159316";
+export const MobileNumber = "0334-2177092";
+export const PersonalEmail = "tanveer.khan2692000@gmail.com";
